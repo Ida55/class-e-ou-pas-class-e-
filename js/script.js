d61f454723be883eec3859d4642cb7f5c345f5e1
Ida55 <50695161+Ida55@users.noreply.github.com>
2019-06-20 14:31:32 +0200

    Add logic for removal of elements when empty button is on

--- js/script.js
+++ js/script.js
@@ -3,34 +3,34 @@
 
 /* le click sur les éléments cercles et carrés génère la coloration de ces éléments (déterminé via la classe 'fill' du document css)*/ 
  $('.btn1').click(function() {
-    $('circle').addClass('fill');  
-    $('rect').addClass('fill');
+    $('circle').addClass('fill1');  
+    $('rect').addClass('fill1');
  });
 
 /* le click sur les éléments cercles et carrés génère le déssemplissage de ces éléments*/
 $('.btn2').click(function() {
-    $('circle').removeClass('fill');
-    $('rect').removeClass('fill');
+    $('circle').removeClass('fill1 fill2 fill3 fill4 fill5 fill6'); /* Merci Nicolas, tous les éléments se vident lorsque le bouton empty est cliqué*/
+    $('rect').removeClass('fill1 fill2 fill3 fill4 fill5 fill6');
 })
 /* les cercles se colorent au 1er click et se décolorent au 2ème (chaque click produit l'éffet contraire au click précédent et donc change l'état des éléments) */
 $('circle').click(function() {
-    $(this).toggleClass('fill'); /* avec (this) on précise bien que ce sont les cercles qui sont concernés */
+    $(this).toggleClass('fill1'); /* avec (this) on précise bien que ce sont les cercles qui sont concernés */
 })
 
 
 $(document).ready(function () { 
     $("#recta").click(function () { 
-        if($("#recta").hasClass("fill")) { 
-            $(".line1").removeClass("fill"); 
+        if($("#recta").hasClass("fill1")) {  /* si la l'élément concerné par l'i.d recta est cliqué et qu'il à la classe fill1 (déterminée dans le CSS)*/
+            $(".line1").removeClass("fill1"); /* on enlève la classe fill1 (couleur orange) des éléments concernés par la classe line1*/
         } else { 
-            $(".line1").addClass("fill2"); 
+            $(".line1").addClass("fill2"); /* ou alors on ajoute la classe fill2 aux éléments concernés par la classe line1*/
     }}); 
 });
 
 $(document).ready(function () { 
     $("#rectb").click(function () { 
-        if($("#rectb").hasClass("fill")) { 
-            $(".line2").removeClass("fill"); 
+        if($("#rectb").hasClass("fill1")) { 
+            $(".line2").removeClass("fill1"); 
         } else { 
             $(".line2").addClass("fill3"); 
     }}); 
@@ -38,8 +38,8 @@ $(document).ready(function () {
 
 $(document).ready(function () { 
     $("#diamond").click(function () { 
-        if($("#diamond").hasClass("fill")) { 
-            $(".line3").removeClass("fill"); 
+        if($("#diamond").hasClass("fill1")) { 
+            $(".line3").removeClass("fill1"); 
         } else { 
             $(".line3").addClass("fill4"); 
     }}); 
@@ -47,8 +47,8 @@ $(document).ready(function () {
 
 $(document).ready(function () { 
     $("#rectc").click(function () { 
-        if($("#rectc").hasClass("fill")) { 
-            $(".line4").removeClass("fill"); 
+        if($("#rectc").hasClass("fill1")) { 
+            $(".line4").removeClass("fill1"); 
         } else { 
             $(".line4").addClass("fill5"); 
     }}); 
@@ -56,40 +56,12 @@ $(document).ready(function () {
 
 $(document).ready(function () { 
     $("#rectd").click(function () { 
-        if($("#rectd").hasClass("fill")) { 
-            $(".line5").removeClass("fill"); 
+        if($("#rectd").hasClass("fill1")) { 
+            $(".line5").removeClass("fill1"); 
         } else { 
             $(".line5").addClass("fill6"); 
     }}); 
 });
-
-// $('rect').click(function() {
-//     if ($(this).toggleClass('fill')) {
-//         $('circle').hasClass('fill2');
-//     } else { 
-//         $(this).removeClass('fill');
-//         $('circle').removeClass('fill2');
-//     }
-// })
-
-
-// $("#recta").click(function() {
-//    if ($(this).hasClass('fill')) {
-//     $('line1').removeClass('fill2');
-//    } else {
-//     $('line1').addClass('fill2');
-//    }
-// });
-
-// $(document).ready(function() {
-//     $(rect#recta.line1).click(function() {
-//         if ($(rect#recta.line1).hasClass('fill')) {
-//         $('line1').removeClass('fill');
-// }  else {
-//     $('line1').addClass('fill');
-// }
-// })
-
 
 
 
